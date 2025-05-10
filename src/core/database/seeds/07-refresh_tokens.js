8c3413267b8f402808d82943f9677ba065606b37
Phạm Tiến <84020540+PhamVanTien1910@users.noreply.github.com>
2025-05-11 00:42:40 +0700

    feat(HN-20): Implement the login feature (#2)

--- src/core/database/seeds/07-refresh_tokens.js
+++ src/core/database/seeds/07-refresh_tokens.js
@@ -3,6 +3,8 @@
  */
 
 exports.seed = async knex => {
+    const now = new Date();
+    const expiresIn7Days = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000);
     await knex('refresh_tokens').del();
 
     await knex('refresh_tokens').insert([
@@ -11,14 +13,14 @@ exports.seed = async knex => {
             token: 'sample_token_1',
             device_info: 'iPhone 13 - Safari',
             created_at: knex.fn.now(),
-            expired_at: knex.fn.now().add(7, 'days'),
+            expired_at: expiresIn7Days,
         },
         {
             user_id: 2,
             token: 'sample_token_2',
             device_info: 'MacBook Pro - Chrome',
             created_at: knex.fn.now(),
-            expired_at: knex.fn.now().add(7, 'days'),
+            expired_at: expiresIn7Days,
         },
     ]);
 };
